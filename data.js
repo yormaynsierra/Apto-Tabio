b40d675951131b4c43b35ed126b571c2d5ddba37
Yormayn Sierra Inmobiliaria <yorsierra9@gmail.com>
2025-10-15 18:25:18 -0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,710 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-entrada",
+      "name": "Entrada",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.811997361169226,
+        "pitch": 0.35013337096489217,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.910853641285458,
+          "pitch": -0.039585557875991384,
+          "rotation": 0,
+          "target": "1-escaleras"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.9176756023234542,
+          "pitch": -0.23043800193018527,
+          "title": "Casaentabio.com",
+          "text": "Contactenos:<div>311 5649032<br></div><div>317 6006453</div>"
+        },
+        {
+          "yaw": 0.013428861061605346,
+          "pitch": -0.03651564199932622,
+          "title": "Casaentabio.com",
+          "text": "Contactenos:<div>311 5649032<br></div><div>317 6006453</div>"
+        }
+      ]
+    },
+    {
+      "id": "1-escaleras",
+      "name": "Escaleras",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.786222611856692,
+        "pitch": 0.7534945533277515,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6965850363642545,
+          "pitch": 0.5511572822469191,
+          "rotation": 0,
+          "target": "0-entrada"
+        },
+        {
+          "yaw": -2.9566520685964193,
+          "pitch": -0.24777051308818088,
+          "rotation": 0,
+          "target": "2-lobby-principal"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 3.051090152020313,
+          "pitch": -0.49751214880478045,
+          "title": "casaentabio.com",
+          "text": "Contactenos:<div>311 5649032<br></div><div>317 6006453</div>"
+        }
+      ]
+    },
+    {
+      "id": "2-lobby-principal",
+      "name": "Lobby Principal",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.9803552315803934,
+        "pitch": 0.3298455938715801,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5910132633443475,
+          "pitch": 0.8069098849630443,
+          "rotation": 0,
+          "target": "1-escaleras"
+        },
+        {
+          "yaw": 2.308714620218237,
+          "pitch": 0.4129726156584539,
+          "rotation": 4.71238898038469,
+          "target": "7-bao-social"
+        },
+        {
+          "yaw": 2.7688909249185842,
+          "pitch": 0.3203213863761256,
+          "rotation": 5.497787143782138,
+          "target": "11-escaleras-3-piso"
+        },
+        {
+          "yaw": -3.1314079310461924,
+          "pitch": 0.3409103347498572,
+          "rotation": 0,
+          "target": "5-taller"
+        },
+        {
+          "yaw": -2.497857996937313,
+          "pitch": 0.2730794101767806,
+          "rotation": 0,
+          "target": "4-sala---comedor"
+        },
+        {
+          "yaw": 0.14033333358039712,
+          "pitch": 0.265536803083668,
+          "rotation": 0,
+          "target": "3-habitacion-principal"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.03332902277561267,
+          "pitch": -0.03553483158934867,
+          "title": "Casaentabio.com",
+          "text": "Contactenos:<div>311 5649032<br></div><div>317 6006453</div>"
+        },
+        {
+          "yaw": -2.4821914811068577,
+          "pitch": -0.06162428886788618,
+          "title": "casaentabio.com",
+          "text": "Contactenos:<div>311 5649032<br></div><div>317 6006453</div>"
+        }
+      ]
+    },
+    {
+      "id": "3-habitacion-principal",
+      "name": "Habitacion Principal",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 3.123332069392843,
+        "pitch": 0.2869459262835985,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.1603947886955588,
+          "pitch": 0.24245311314043505,
+          "rotation": 0,
+          "target": "9-bao-principal"
+        },
+        {
+          "yaw": 1.5717230675682385,
+          "pitch": 0.2212699050779463,
+          "rotation": 0,
+          "target": "2-lobby-principal"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.553167291688891,
+          "pitch": -0.047059809811154096,
+          "title": "casaentabio.com",
+          "text": "Contactenos:<div>311 5649032<br></div><div>3176006453</div>"
+        }
+      ]
+    },
+    {
+      "id": "4-sala---comedor",
+      "name": "Sala - Comedor",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.34243910408456557,
+        "pitch": 0.4296389374301519,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6100780583929737,
+          "pitch": 0.30706725331299545,
+          "rotation": 0,
+          "target": "2-lobby-principal"
+        },
+        {
+          "yaw": 1.0572949956789977,
+          "pitch": 0.2785429035765752,
+          "rotation": 0,
+          "target": "11-escaleras-3-piso"
+        },
+        {
+          "yaw": 1.2740384299274439,
+          "pitch": 0.5218388153355242,
+          "rotation": 7.853981633974483,
+          "target": "5-taller"
+        },
+        {
+          "yaw": -2.7966632769374797,
+          "pitch": 0.11073535912004218,
+          "rotation": 0,
+          "target": "6-cocina"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.07534917155594734,
+          "pitch": 0.8203772086570034,
+          "title": "casaentabio.com",
+          "text": "Contactenos:<div>311 5649032<br></div><div>317 6006453</div>"
+        },
+        {
+          "yaw": -2.822520798085659,
+          "pitch": -0.18536310572496717,
+          "title": "casaentabio.com",
+          "text": "Contactenos:<div>311 5649032<br></div><div>317 6006453</div>"
+        }
+      ]
+    },
+    {
+      "id": "5-taller",
+      "name": "Taller",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.9256312679303456,
+        "pitch": 0.4049718464503389,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.40454942905564906,
+          "pitch": 0.2454225742142775,
+          "rotation": 0.7853981633974483,
+          "target": "2-lobby-principal"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.3706395720710738,
+          "pitch": 0.027049823220645308,
+          "title": "casaentabio.com",
+          "text": "Contactenos:<div>311 5649032<br></div><div>317 6006453</div>"
+        }
+      ]
+    },
+    {
+      "id": "6-cocina",
+      "name": "Cocina",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -1.4846006797038136,
+        "pitch": 0.4840350765394241,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.13651093363218791,
+          "pitch": 0.1379523115359973,
+          "rotation": 0,
+          "target": "4-sala---comedor"
+        },
+        {
+          "yaw": 2.9827407784802595,
+          "pitch": 0.2186026303822004,
+          "rotation": 0,
+          "target": "8-area-de-lavado"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.10079352647453987,
+          "pitch": -0.2798047322839352,
+          "title": "casaentabio.com",
+          "text": "Contactenos:<div>311 5649032<br></div><div>317 6006453</div>"
+        },
+        {
+          "yaw": 2.82625310749831,
+          "pitch": -0.03546184470988756,
+          "title": "casaentabio.com",
+          "text": "Contactenos:<div>311 5649032<br></div><div>317 6006453</div>"
+        }
+      ]
+    },
+    {
+      "id": "7-bao-social",
+      "name": "Baño Social",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.9149331455899521,
+        "pitch": 0.48882849737345424,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9622400720722712,
+          "pitch": 0.43419886406599595,
+          "rotation": 0,
+          "target": "2-lobby-principal"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.8914855343038184,
+          "pitch": 0.12141579607613373,
+          "title": "casaentabio.com",
+          "text": "Contactenos:<div>311 5649032<br></div><div>317 6006453</div>"
+        }
+      ]
+    },
+    {
+      "id": "8-area-de-lavado",
+      "name": "Area de Lavado",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.88342936031417,
+        "pitch": 0.3860706613061531,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.692255962628506,
+          "pitch": 0.11054244164660432,
+          "rotation": 0,
+          "target": "6-cocina"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.6490018133706883,
+          "pitch": 0.3704667769404182,
+          "title": "casaentabio.com",
+          "text": "Contactenos:<div>311 5649032<br></div><div>317 6006453</div>"
+        }
+      ]
+    },
+    {
+      "id": "9-bao-principal",
+      "name": "Baño Principal",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.659620894236852,
+        "pitch": 0.6661121416612552,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4237594772275557,
+          "pitch": 0.5272965493358548,
+          "rotation": 0,
+          "target": "3-habitacion-principal"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.666302891969556,
+          "pitch": 0.266959030561571,
+          "title": "casaentabio.com",
+          "text": "<div>CONTACTENOS</div><div>311 5649032<br></div>317 6006453<div><br></div>"
+        }
+      ]
+    },
+    {
+      "id": "10-lobby-3-piso",
+      "name": "Lobby 3 Piso",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -3.118347277610212,
+        "pitch": 0.4755758588494494,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.613486414596439,
+          "pitch": 0.3030061280256433,
+          "rotation": 0,
+          "target": "12-estudio"
+        },
+        {
+          "yaw": -2.3574707072925136,
+          "pitch": 1.0824479304143964,
+          "rotation": 0,
+          "target": "11-escaleras-3-piso"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.733977230484623,
+          "pitch": 0.3277453772503538,
+          "title": "casaentabio.com",
+          "text": "Contactenos:<div>311 5649032<br></div><div>317 6006453</div>"
+        }
+      ]
+    },
+    {
+      "id": "11-escaleras-3-piso",
+      "name": "Escaleras 3 Piso",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.764325587925107,
+        "pitch": 0.7879467693562852,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0594753972365067,
+          "pitch": 0.7132121126977928,
+          "rotation": 4.71238898038469,
+          "target": "5-taller"
+        },
+        {
+          "yaw": -2.6391393085597343,
+          "pitch": 0.581657119739674,
+          "rotation": 0,
+          "target": "4-sala---comedor"
+        },
+        {
+          "yaw": -1.8205399000197282,
+          "pitch": 0.8967994483401895,
+          "rotation": 1.5707963267948966,
+          "target": "2-lobby-principal"
+        },
+        {
+          "yaw": 1.6829266688981024,
+          "pitch": -0.5964025326038129,
+          "rotation": 0,
+          "target": "10-lobby-3-piso"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.6540568350114704,
+          "pitch": 0.2258391846969996,
+          "title": "casaentabio.com",
+          "text": "Contactenos:<div>311 5649032<br></div><div>317 6006453</div>"
+        }
+      ]
+    },
+    {
+      "id": "12-estudio",
+      "name": "Estudio",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -1.3095762729384752,
+        "pitch": 0.5455732538795601,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0739291527516865,
+          "pitch": 0.2647042459160289,
+          "rotation": 4.71238898038469,
+          "target": "10-lobby-3-piso"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.470839011526568,
+          "pitch": 0.8708492368244549,
+          "title": "casaentabio.com",
+          "text": "Contactenos:<div>311 5649032<br></div><div>317 6006453</div>"
+        }
+      ]
+    }
+  ],
+  "name": "Apto Tabio",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
